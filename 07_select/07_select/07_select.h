4b5579ba8fb04e80e3940efac63c70f5fee398c6
grb2015 <9494909706@qq.com>
2021-11-16 16:02:12 +0800

    [add] 上传下拉框的示例，刚刚创建

---
+++ 07_select/07_select/07_select.h
@@ -0,0 +1,32 @@
+﻿
+// 07_select.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMy07selectApp:
+// 有关此类的实现，请参阅 07_select.cpp
+//
+
+class CMy07selectApp : public CWinApp
+{
+public:
+	CMy07selectApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMy07selectApp theApp;
---
+++ grb_demo/grb_demo/grb_demoDlg.h
@@ -0,0 +1,36 @@
+﻿
+// grb_demoDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CgrbdemoDlg 对话框
+class CgrbdemoDlg : public CDialogEx
+{
+// 构造
+public:
+	CgrbdemoDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_GRB_DEMO_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CListCtrl m_list;
+};
